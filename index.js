dadf006a639fa89b152b2af22be9dc197cfe916e
Afsaruddin12133 <afsaruddin12133@gmail.com>
2022-07-03 09:39:01 +0600

    update last

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const port = 3000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.send('Afsar uddin');
+  })
+
+
+const uri = `mongodb+srv://productcountdb:${process.env.MD_PASS}@cluster0.ucc3fpr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// api....
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
